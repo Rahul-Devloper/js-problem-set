79d8e27d52d83ecf2fbb7e3a0f3410b6828b7612
Rahul <rahulramesh26@yahoo.com>
2020-01-05 19:56:21 +0530

    scope

--- js problem.js
+++ js problem.js
@@ -28,3 +28,20 @@ function kebabToSnake(str) {
     var newStr = str.replace(/ - /g  , "_");
     return newStr;
 }
+/*scope*/
+var x=2;
+console.log(x);
+function math(){
+    var x=3;
+    return x;
+}
+/*scope that can access the values outside the function and change it*/
+var y=4;
+console.log(y);
+function moreMath(){
+    return y;
+}
+function anotherMath(){
+    y=5;
+    return y;
+}
